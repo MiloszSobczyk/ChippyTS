0f15a34e8155cc1d6bc28857853ea4a382593275
MiloszSobczyk <m.sobczyk.official@gmail.com>
2025-11-10 15:40:02 +0100

    Setup npm packages and configure path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  resolve: {
+    alias: {
+      "@core": path.resolve(__dirname, "./src/core"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+    },
+  },
+});
